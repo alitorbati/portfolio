c5e57b98e6c9733e8bafae1afef8104ccf977f50
Ali Torbati <ali.torbati@gmail.com>
2020-04-25 20:07:03 -0700

    bump deps

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -1,43 +1,41 @@
-import React, { Component } from "react";
+import React from "react";
 import Box from "../components/Box";
 import { Link } from "react-router-dom";
 
-class Contact extends Component {
-  render() {
-    const primarySocialLinks = [
-      "http://twitter.com/alitorbati",
-      "http://github.com/alitorbati",
-    ];
-    const secondarySocialLinks = [
-      "mailto:ali.torbati@gmail.com",
-      "http://instagram.com/alitorbati",
-      "http://linkedin.com/in/alitorbati",
-      "http://behance.net/alitorbati",
-    ];
+const Contact = (props) => {
+  const primarySocialLinks = [
+    "http://twitter.com/alitorbati",
+    "http://github.com/alitorbati",
+  ];
+  const secondarySocialLinks = [
+    "mailto:ali.torbati@gmail.com",
+    "http://instagram.com/alitorbati",
+    "http://linkedin.com/in/alitorbati",
+    "http://behance.net/alitorbati",
+  ];
 
-    return (
-      <Box>
-        <Box as="ul" marginBottom={2}>
-          {primarySocialLinks.map((link) => (
-            <li key={link}>
-              <Link to={link} target="_blank">
-                {link}
-              </Link>
-            </li>
-          ))}
-        </Box>
-        <Box as="ul">
-          {secondarySocialLinks.map((link) => (
-            <li key={link}>
-              <Link to={link} target="_blank">
-                {link}
-              </Link>
-            </li>
-          ))}
-        </Box>
+  return (
+    <Box>
+      <Box as="ul" marginBottom={2}>
+        {primarySocialLinks.map((link) => (
+          <li key={link}>
+            <Link to={link} target="_blank">
+              {link}
+            </Link>
+          </li>
+        ))}
       </Box>
-    );
-  }
-}
+      <Box as="ul">
+        {secondarySocialLinks.map((link) => (
+          <li key={link}>
+            <Link to={link} target="_blank">
+              {link}
+            </Link>
+          </li>
+        ))}
+      </Box>
+    </Box>
+  );
+};
 
 export default Contact;
