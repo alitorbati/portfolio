c6144ac12cb46b6b98fcb95a985dc543b55bbc97
Ali Torbati <ali.torbati@gmail.com>
2020-04-25 19:29:46 -0700

    use fetch instead of axios

--- src/pages/Projects.js
+++ src/pages/Projects.js
@@ -1,62 +1,62 @@
-import React, { Component } from "react";
+import React, { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
-import axios from "axios";
 import Text from "../components/Text";
 import Box from "../components/Box";
 import CleanSheetData from "../components/CleanSheetData";
-// import kineticType from '../images/kinetic-type.gif'
 
-class Projects extends Component {
-  constructor(props) {
-    super(props);
-    this.state = { data: [] };
-  }
+const Projects = (props) => {
+  const [projects, setProjects] = useState([]);
+  const url =
+    "https://spreadsheets.google.com/feeds/list/12sERGaYvU1ZUsEOnG11LgR8ZQVtLW3zn2Kv8yOOBPyg/od6/public/values?alt=json";
 
-  componentDidMount() {
-    const url =
-      "https://spreadsheets.google.com/feeds/list/12sERGaYvU1ZUsEOnG11LgR8ZQVtLW3zn2Kv8yOOBPyg/od6/public/values?alt=json";
-    axios.get(url).then((res) => {
-      this.setState({ data: res.data.feed.entry });
-    });
-  }
+  useEffect(() => {
+    const abortController = new AbortController();
 
-  render() {
-    if (!this.state.data) return null;
+    async function fetchData() {
+      const res = await fetch(url, { signal: abortController.signal });
+      res.json().then((data) => setProjects(data.feed.entry));
+    }
 
-    return (
-      <CleanSheetData data={this.state.data}>
-        {({ data }) =>
-          data
-            .filter((x) => x.show.toLowerCase() === "true")
-            .map((x, i) => (
-              <Box key={i} marginBottom={3}>
-                <Text
-                  as={Link}
-                  to={x.href}
-                  target="_blank"
-                  fontSize={[1, 2]}
-                  fontWeight={700}
-                  style={{ position: "sticky", top: 0 }}
-                >
-                  {x.title}
-                </Text>
-                {/*
-                  <Box>
-                    <img src={ kineticType }/>
-                  </Box>
-                  */}
-                <Box marginBottom={1}>
-                  <Text fontWeight={700}>{x.position}</Text>
+    fetchData();
+
+    return () => {
+      abortController.abort();
+    };
+  }, [url]);
+
+  return (
+    <CleanSheetData data={projects}>
+      {({ data }) =>
+        data
+          .filter((x) => x.show.toLowerCase() === "true")
+          .map((x, i) => (
+            <Box key={i} marginBottom={3}>
+              <Text
+                as={Link}
+                to={x.href}
+                target="_blank"
+                fontSize={[1, 2]}
+                fontWeight={700}
+                style={{ position: "sticky", top: 0 }}
+              >
+                {x.title}
+              </Text>
+              {/*
+                <Box>
+                  <img src={ kineticType }/>
                 </Box>
-                <Text as="p" marginBottom={1}>
-                  {x.description}
-                </Text>
+                */}
+              <Box marginBottom={1}>
+                <Text fontWeight={700}>{x.position}</Text>
               </Box>
-            ))
-        }
-      </CleanSheetData>
-    );
-  }
-}
+              <Text as="p" marginBottom={1}>
+                {x.description}
+              </Text>
+            </Box>
+          ))
+      }
+    </CleanSheetData>
+  );
+};
 
 export default Projects;
