e4d025f0a2332e77936e83017f413a69175bdfb5
Ali Torbati <ali.torbati@gmail.com>
2020-08-18 18:29:56 -0700

    new build

---
+++ build/precache-manifest.ea507f6405904fb9cf0f0fdc9564bb26.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c528f8c012ae55f2d215ac5409b8a27f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5e1e4a50cfe764fa3182",
+    "url": "/static/js/2.fc803543.chunk.js"
+  },
+  {
+    "revision": "507c4436aa55e48136618e82a79cbf78",
+    "url": "/static/js/2.fc803543.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0c7c409ca5a5b0566981",
+    "url": "/static/js/main.5a794379.chunk.js"
+  },
+  {
+    "revision": "b775e3132e0930477ff9",
+    "url": "/static/js/runtime-main.17a0efde.js"
+  }
+]);
