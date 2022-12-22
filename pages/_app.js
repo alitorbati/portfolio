cba3b161248eb4f02262e4f9dfdb968e0d521a16
Ali Torbati <ali.torbati@gmail.com>
2022-12-22 12:27:22 -0800

    remove service worker stuff

--- pages/_app.js
+++ pages/_app.js
@@ -37,57 +37,22 @@ const components = {
 const App = (props) => {
   const { Component, pageProps } = props;
 
-  if (
-    typeof window !== "undefined" &&
-    typeof window.navigator !== "undefined" &&
-    window &&
-    window.navigator &&
-    navigator.serviceWorker
-  ) {
-    console.log("unregister service workers");
-    navigator.serviceWorker.getRegistrations().then(function (registrations) {
-      for (let registration of registrations) {
-        registration.unregister();
-      }
-    });
-  }
-
-  React.useEffect(() => {
-    if ("serviceWorker" in navigator) {
-      console.log("register no-op service worker");
-      window.addEventListener("load", function () {
-        navigator.serviceWorker.register("/service-worker.js").then(
-          function (registration) {
-            console.log(
-              "Service Worker registration successful with scope: ",
-              registration.scope
-            );
-          },
-          function (err) {
-            console.log("Service Worker registration failed: ", err);
-          }
-        );
-      });
-    }
-  }, []);
-
   const [theme, setTheme] = React.useState(dark);
 
-  // initialize theme
   React.useEffect(() => {
-    window.matchMedia &&
-    window.matchMedia("(prefers-color-scheme: dark)").matches
-      ? setTheme(dark)
-      : setTheme(light);
-  }, []);
+    if (!window.matchMedia) {
+      setTheme(dark);
+    }
 
-  // change theme when device theme changes
-  React.useEffect(() => {
-    window
-      .matchMedia("(prefers-color-scheme: dark)")
-      .addEventListener("change", (event) => {
-        event.matches ? setTheme(dark) : setTheme(light);
-      });
+    const mm = window.matchMedia("(prefers-color-scheme: dark)");
+
+    // initialize theme
+    mm.matches ? setTheme(dark) : setTheme(light);
+
+    // change theme when device theme changes
+    mm.addEventListener("change", (event) => {
+      event.matches ? setTheme(dark) : setTheme(light);
+    });
   }, []);
 
   const router = useRouter();
