746979a13fcedfe0a8961ad6b592c1663cda69f2
Ali Torbati <ali.torbati@gmail.com>
2023-02-27 17:27:44 -0800

    use flexbox instead of grid

--- pages/sketches/index.js
+++ pages/sketches/index.js
@@ -1,14 +1,14 @@
 import path from "path";
 import { sortByDate } from "../../utils/sortByDate";
 import { getAllPosts } from "../../utils/getAllPosts";
-import Grid from "../../components/foundations/Grid";
+import Flexbox from "../../components/foundations/Flexbox";
 import CollectionItem from "../../components/CollectionItem";
 
 const Sketches = (props) => {
   const { posts } = props;
 
   return (
-    <Grid gap={5} gridTemplateColumns={["1fr", "repeat(2, 1fr)"]}>
+    <Flexbox flexDirection="column" gap={5}>
       {posts.map((post) => {
         const href = path.join("sketches", post.slug);
 
@@ -20,7 +20,7 @@ const Sketches = (props) => {
           />
         );
       })}
-    </Grid>
+    </Flexbox>
   );
 };
 
