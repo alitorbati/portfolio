02bc71b6ce5cc79fe6ec6e6e7c32097d2df51cac
Ali Torbati <ali.torbati@gmail.com>
2023-04-26 21:22:33 -0700

    career page

--- pages/career.js
+++ pages/career.js
@@ -8,15 +8,23 @@ import Flexbox from "../components/foundations/Flexbox";
 import React from "react";
 
 const jobs = [
-  // {
-  //   company: "ArtCenter",
-  //   href: "https://www.artcenter.edu/",
-  //   positions: ["Assistant Professor"],
-  //   start: "2022-09-01",
-  //   end: "Present",
-  //   description:
-  //     "Teaching generative design and transmedia at one of the world's most prestigious design schools",
-  // },
+  {
+    company: "Impart Security",
+    href: "https://www.impart.security/",
+    positions: ["Senior Software Engineer"],
+    start: "2023-04-18",
+    end: "Present",
+    description: "",
+  },
+  {
+    company: "ArtCenter",
+    href: "https://www.artcenter.edu/",
+    positions: ["Assistant Professor"],
+    start: "2022-09-01",
+    end: "Present",
+    description:
+      "Teaching generative design and transmedia at one of the world's most prestigious design schools",
+  },
   {
     company: "Fastly",
     href: "http://fastly.com",
@@ -44,24 +52,24 @@ const jobs = [
     description:
       "Created and maintained consistent, scalable, UI components. Provided technical consultation to design and product teams.",
   },
-  {
-    company: "Rosetta",
-    href: "http://rosetta.com",
-    positions: ["Intern", "UI Developer"],
-    start: "2013-04-20",
-    end: "2013-11-15",
-    description:
-      "Created and distributed branded assets online. Survived the deep, dark ocean of buzz words and marketing lingo.",
-  },
-  {
-    company: "DeviantART",
-    href: "http://deviantart.com",
-    positions: ["Intern"],
-    start: "2011-07-01",
-    end: "2011-09-15",
-    description:
-      "Grew the world's largest online art community by researching and presenting strengths/weaknesses of several new products under consideration by an advisory committee.",
-  },
+  // {
+  //   company: "Rosetta",
+  //   href: "http://rosetta.com",
+  //   positions: ["Intern", "UI Developer"],
+  //   start: "2013-04-20",
+  //   end: "2013-11-15",
+  //   description:
+  //     "Created and distributed branded assets online. Survived the deep, dark ocean of buzz words and marketing lingo.",
+  // },
+  // {
+  //   company: "DeviantART",
+  //   href: "http://deviantart.com",
+  //   positions: ["Intern"],
+  //   start: "2011-07-01",
+  //   end: "2011-09-15",
+  //   description:
+  //     "Grew the world's largest online art community by researching and presenting strengths/weaknesses of several new products under consideration by an advisory committee.",
+  // },
   {
     company: "Cal Poly, San Luis Obispo",
     href: "http://calpoly.com",
